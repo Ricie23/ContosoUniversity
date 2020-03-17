dc58203ee4c39d77b6ef1b3059b26bae97f29770
Richard Robertson <Rich.Robertson2@outlook.com>
2020-03-17 16:32:05 -0400

    Lesson 10 Compete

---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        [Required]
+        [ConcurrencyCheck]
+        [MaxLength(24, ErrorMessage ="The maxium length is 24 characters")]
+        [MinLength(5, ErrorMessage ="The mininum length is 5 characters")]
+        [Index(IsUnique =true)]
+        public string Title { get; set; }
+        [Range(1,6)]
+        public int Credits { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
